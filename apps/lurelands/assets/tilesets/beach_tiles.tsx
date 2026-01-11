de0f2d87e7dbc6d9fab3614c2a56260870424d9c
Jimmy Arroyo <jamesarroyo5@gmail.com>
2026-01-11 01:14:41 -0700

    Update terrain assets and tilesets for improved game visuals
    - Added new terrain images including beach, nature, and water spritesheets to enhance the game's visual environment.
    - Updated existing tilesets to reference the new images, ensuring consistency across the game assets.
    - Removed outdated tilesets related to previous assets, streamlining the asset management process.
    - Adjusted map structure in map1.tmx to incorporate the new tilesets, improving gameplay experience.

--- apps/lurelands/assets/tilesets/beach_tiles.tsx
+++ apps/lurelands/assets/tilesets/beach_tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Beach Tiles" tilewidth="16" tileheight="16" tilecount="84" columns="12">
- <image source="C:/Users/boxca/Downloads/Pixelwood Valley 1.1.1 (1)/Pixelwood Valley 1.1.1/Beach/Tiles/Tiles.png" width="192" height="112"/>
+<tileset version="1.10" tiledversion="1.11.2" name="beach_tilesheet" tilewidth="16" tileheight="16" tilecount="84" columns="12">
+ <image source="../images/terrain/beach_spritesheet.png" width="192" height="112"/>
 </tileset>
--- apps/lurelands/assets/tilesets/beach_tree.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Tree" tilewidth="16" tileheight="16" tilecount="4" columns="2">
- <image source="C:/Users/boxca/Downloads/Pixelwood Valley 1.1.1 (1)/Pixelwood Valley 1.1.1/Beach/Tree/Tree.png" width="38" height="44"/>
-</tileset>
