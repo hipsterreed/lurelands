4cd23bee9597825b805dea1c984d53cdcab115b8
Jimmy Arroyo <jamesarroyo5@gmail.com>
2026-01-11 01:45:01 -0700

    Add new color palette assets and update map structure
    - Introduced new color palette images and tileset to enhance visual elements in the game.
    - Updated map1.tmx to incorporate the new color palette tileset, improving gameplay aesthetics.
    - Adjusted layer visibility settings and added properties for better asset management and gameplay experience.

--- apps/lurelands/assets/tilesets/nature_tiles.tsx
+++ apps/lurelands/assets/tilesets/nature_tiles.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="nature_tiles" tilewidth="16" tileheight="16" tilecount="152" columns="19">
  <image source="../images/terrain/nature.png" width="304" height="128"/>
+ <tile id="61">
+  <properties>
+   <property name="is_walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
 </tileset>
