125aa6f3577930b1f280b3d1f518c66eb2ddd784
Jimmy Arroyo <jamesarroyo5@gmail.com>
2026-01-10 23:16:32 -0700

    Add signs tileset and update map structure
    - Introduced a new tileset for signs in signs.tsx, featuring three distinct sign images.
    - Updated map1.tmx to include the new signs tileset and reorganized layers for better structure, including renaming layers and adjusting visibility settings.
    - Enhanced the ground layer visibility and added a new water layer to improve map detail and gameplay experience.

---
+++ apps/lurelands/assets/images/structures/signs.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="signs" tilewidth="32" tileheight="27" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../../../../../../purchased_content/sprites/Nature Pixels V2 Rural Retreat/Wooden/Table.png" width="32" height="25"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../../../../../../purchased_content/sprites/Nature Pixels V2 Rural Retreat/Wooden/RighLeft.png" width="20" height="27"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../../../../../../purchased_content/sprites/Nature Pixels V2 Rural Retreat/Wooden/Sign.png" width="21" height="25"/>
+ </tile>
+</tileset>
